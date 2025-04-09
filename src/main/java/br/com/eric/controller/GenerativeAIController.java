ed6d5943786bdcf654c893ec40fc283b681b840b
EricBernardi <eric.gomes.bernardi@gmail.com>
2025-04-09 01:09:40 -0300

    feat: add service to generate images and config to cors

--- src/main/java/br/com/eric/controller/GenerativeAIController.java
+++ src/main/java/br/com/eric/controller/GenerativeAIController.java
@@ -1,20 +1,27 @@
 package br.com.eric.controller;
 
 import br.com.eric.service.ChatService;
+import br.com.eric.service.ImageService;
 import br.com.eric.service.RecipeService;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GenerativeAIController {
 
     private final ChatService chatService;
     private final RecipeService recipeService;
+    private final ImageService imageService;
 
-    public GenerativeAIController(ChatService chatService, RecipeService recipeService) {
+    public GenerativeAIController(ChatService chatService, RecipeService recipeService,
+                                  ImageService imageService) {
         this.chatService = chatService;
         this.recipeService = recipeService;
+        this.imageService = imageService;
     }
 
     @GetMapping("ask-ai")
@@ -34,5 +41,19 @@ public class GenerativeAIController {
 
         return recipeService.createRecipe(ingredients, cuisine, dietaryRestrictions);
 
+    }
+
+    @GetMapping("image-generator")
+    public List<String> createImage(@RequestParam String prompt,
+                                    @RequestParam(defaultValue = "hd") String quality,
+                                    @RequestParam(defaultValue = "1") Integer n,
+                                    @RequestParam(defaultValue = "1024") Integer height,
+                                    @RequestParam(defaultValue = "1024") Integer width){
+
+        ImageResponse response = imageService.generateImage(prompt, quality, n, height, width);
+        List<String> imageUrls = response.getResults().stream()
+                .map(result -> result.getOutput().getUrl())
+                .toList();
+        return imageUrls;
     }
 }
